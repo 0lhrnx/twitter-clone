be162b72f360b64b51dc35310287d41279fb27c9
leesan <s24046@gsm.hs.kr>
2025-03-18 11:09:50 +0900

    firebase

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBibmGEpTTkppIKX_lc4dlYXmXBevgBlUc",
+  authDomain: "twitter-clone-e10a4.firebaseapp.com",
+  projectId: "twitter-clone-e10a4",
+  storageBucket: "twitter-clone-e10a4.firebasestorage.app",
+  messagingSenderId: "565221893050",
+  appId: "1:565221893050:web:e1acebbc8086633abdf383",
+};
+
+const app = initializeApp(firebaseConfig);
